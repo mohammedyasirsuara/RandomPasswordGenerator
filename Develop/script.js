ecd40ffa800d1d3780bcfefa0ba16e5a4391cccf
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-05-31 21:39:46 -0400

    Added the generate password function

---
+++ Develop/script.js
@@ -0,0 +1,58 @@
+//Function to generate password 
+function generatePassword(){
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const special = "~!@#$%&^*()-_+={}[]\|/;:<>,.?";
+  const number = "1234567890";
+  let nPassword="";
+  let password="";
+  
+  
+  var pLength = parseInt(prompt("Pick a password length between 8 and 128"));
+  if(pLength<8 || pLength>128){
+    alert("You need to enter a number between 8 and 128");
+    generatePassword();
+
+  }else{
+    var Lcase = confirm("Do you want the password to contain Lower case?");
+    var Ucase = confirm("Do you want the password to contain Upper case?");
+    var sLetters = confirm("Do you want the password to contain Special characters?");
+    var num = confirm("Do you want the password to contain numbers?");
+
+    if(Ucase){
+      nPassword+=uppercase;
+    }if(Lcase){
+      nPassword+=lowercase;
+    }if(sLetters){
+      nPassword+=special;
+    }if(num){
+      nPassword+=number;
+    }
+    if(!num&&!Lcase&&!Ucase&&!sLetters){
+      alert("You need to select a password criteria")
+      generatePassword();
+    }
+
+    while(0!=pLength){
+      password += nPassword.charAt(Math.floor(Math.random()*nPassword.length));
+      pLength = pLength-1;
+    }
+  }
+  
+  return password;
+}
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
